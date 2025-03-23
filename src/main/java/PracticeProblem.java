3f8937dff25e19a92f3c11d453ee86fb023d7ce0
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-03-23 20:08:13 +0000

    Final Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,49 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) {}
 
+	public static void selectionSortName(String[] names, int[] ages) {
+		
+		for (int i = 0; i < names.length - 1; i++) {
+			
+			int smallestIndex = i;
+
+			for (int j = i+1; j < names.length; j++) {
+				if (names[j].compareToIgnoreCase(names[smallestIndex]) < 0) {
+					smallestIndex = j;
+				}
+			}
+
+			String nameTemp = names[smallestIndex];
+			names[smallestIndex] = names[i];
+			names[i] = nameTemp;
+			
+			int ageTemp = ages[smallestIndex];
+			ages[smallestIndex] = ages[i];
+			ages[i] = ageTemp;
+		}
 	}
 
-	
+	public static void selectionSortAge(String[] names, int[] ages) {
+		
+		for (int i = 0; i < ages.length - 1; i++) {
+			
+			int smallestIndex = i;
+			
+			for (int j = i+1; j < ages.length; j++) {
+				if (ages[j] < ages[smallestIndex]) {
+					smallestIndex = j;
+				}
+			}
+
+			int ageTemp = ages[smallestIndex];
+			ages[smallestIndex] = ages[i];
+			ages[i] = ageTemp;
+
+			String nameTemp = names[smallestIndex];
+			names[smallestIndex] = names[i];
+			names[i] = nameTemp;
+		}
+	}
 
 }
